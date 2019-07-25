f64971ab05d0e283a234187d0f2d4241c6f5692d
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-07-24 21:53:23 -0500

    Allowed for blocks to be added to the DOM when the correct button is pressed

--- script.js
+++ script.js
@@ -4,25 +4,49 @@ console.log('js');
 
 let redCount = 0;
 let yellowCount = 0;
+let greenCount = 0;
+let blueCount = 0;
 
 function readyNow(){
-$('#redButton').on('click', addRedCount);
-$('#yellowButton').on('click', addYellowCount);
+$('#redButton').on('click', addRed);
+$('#yellowButton').on('click', addYellow);
+$('#greenButton').on('click', addGreen);
+$('#blueButton').on('click', addBlue);
 }
 
 //add to count on html when corresponding button is clicked
-function addRedCount(){
+function addRed(){
     console.log('Red');
     redCount++;
     let reds = $('#redCount');
     reds.empty();
     reds.append(redCount);
-} //end addRedCount
+    $('#blocks').append(`<div class= "redBlock" >` + `</div>`);
+} //end addRed
 
-function addYellowCount() {
+function addYellow() {
     console.log('Yellow');
     yellowCount++;
     let yellows = $('#yellowCount');
     yellows.empty();
     yellows.append(yellowCount);
-}
+    $('#blocks').append(`<div class= "yellowBlock" >` + `</div>`);
+} //end addYellow
+
+function addGreen() {
+    console.log('Green');
+    greenCount++;
+    let greens = $('#greenCount');
+    greens.empty();
+    greens.append(greenCount);
+    $('#blocks').append(`<div class= "greenBlock" >` + `</div>`);
+} //end addGreen
+
+function addBlue() {
+    console.log('Blue');
+    blueCount++;
+    let blues = $('#blueCount');
+    blues.empty();
+    blues.append(blueCount);
+    $('#blocks').append(`<div class= "blueBlock" >` + `</div>`);
+} //end addBlue
